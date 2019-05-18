e2df9bb74a7adfc548cc18a4c293b5cde26633cd
Dennis Ruhe <dennis@amazingsystems.nl>
2019-05-18 16:01:21 +0200

    Fixed ticker

--- components/mixins/TickerMixin.ts
+++ components/mixins/TickerMixin.ts
@@ -1,15 +1,20 @@
+import axios from 'axios'
 import { Component, Vue } from 'vue-property-decorator'
 
 export const TICKER_URL = 'https://api.coinmarketcap.com/v1/ticker/e-gulden/?convert=EUR'
 
 export interface TickerInfo {
+  symbol: string
+  price_btc: number
+  price_eur: number
+  market_cap_eur: number
+  '24h_volume_eur': number
 }
 
 @Component
 class TickerMixin extends Vue {
-
   async getTickerInfo() {
-    const response = await this.$axios.$get(TICKER_URL)
+    const response = await axios.get(TICKER_URL)
     return <TickerInfo> response[0]
   }
 }
