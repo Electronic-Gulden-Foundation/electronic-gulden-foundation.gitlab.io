e85601c5f14f8748a1711860e168815dbcaab7a7
Dennis Ruhe <dennis@amazingsystems.nl>
2019-07-21 12:15:47 +0200

    Fixed TS errors

--- locales/news/index.ts
+++ locales/news/index.ts
@@ -7,5 +7,12 @@ export interface NewsItem {
 }
 
 export const getAllNewsItems = (): NewsItem[] => {
-  return NewsJson
+  return NewsJson.map(
+    (item): NewsItem => {
+      return {
+        ...item,
+        date: new Date(item.date)
+      }
+    }
+  ).sort((a, b) => a.date.getTime() - b.date.getTime())
 }
