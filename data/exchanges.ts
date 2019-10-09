b116eecb49cee77787f272498816549dadc80188
Dennis Ruhe <dennis@amazingsystems.nl>
2019-10-09 07:53:00 +0000

    Removed NovaExchange

--- data/exchanges.ts
+++ data/exchanges.ts
@@ -9,11 +9,5 @@ export default [
     href: 'https://www.altilly.com/market/EFL_BTC',
     image:
       'https://pbs.twimg.com/profile_images/1069218760509145090/-oNvv4Hj_400x400.jpg'
-  },
-  {
-    name: 'Novaexchange',
-    href: 'https://novaexchange.com/market/BTC_EFL/',
-    image:
-      'https://pbs.twimg.com/profile_images/1045596482513055747/ozE-hjtP_400x400.jpg'
   }
 ]
