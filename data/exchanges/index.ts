b99d637e5ac8aeaaf95a2ab5cc85e2605dcff014
Dennis Ruhe <dennis@amazingsystems.nl>
2020-03-21 13:09:36 +0100

    Added folgory exchange

--- data/exchanges/index.ts
+++ data/exchanges/index.ts
@@ -1,17 +1,17 @@
 export default [
   {
-    name: 'Guldentrader',
-    href: 'https://exchange.guldentrader.com/markets/preview/EUR-EFL',
-    image: require('./guldentrader.png')
-  },
-  {
     name: 'Altilly',
     href: 'https://www.altilly.com/market/EFL_BTC',
-    image: require('./altilly.jpg')
+    image: require('./img/altilly.jpg')
+  },
+  {
+    name: 'Folgory',
+    href: 'https://folgory.com/trade-crypto/EFL_BTC',
+    image: require('./img/folgory.jpg')
   },
   {
     name: 'TradecoinX',
     href: 'https://trade.tradecoinx.ca/trading/eflbtc',
-    image: require('./tradecoinx.png')
+    image: require('./img/tradecoinx.png')
   }
 ]
