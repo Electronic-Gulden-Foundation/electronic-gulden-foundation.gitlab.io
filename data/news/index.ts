f7cd0e669c7fea101907688f1be9e99f517fe7a7
Dennis Ruhe <dennis@amazingsystems.nl>
2020-03-21 13:04:07 +0100

    Lint fix

--- data/news/index.ts
+++ data/news/index.ts
@@ -7,26 +7,26 @@ export interface NewsItem {
   link: string
   isExternalLink: boolean
 
-  originalIndex: number|undefined
+  originalIndex: number | undefined
 }
 
-const itemsWithIndex: NewsItem[] = (<any> NewsJson)
+const itemsWithIndex: NewsItem[] = (<any>NewsJson)
   .map((item: NewsItem, index: number) => {
     item.originalIndex = index
     return item
   })
 
-const indexedByLink = itemsWithIndex.reduce((map, item) => {
-  map[item.link] = item
+const indexedByLink: Map<string, NewsItem> = itemsWithIndex.reduce((map, item) => {
+  map.set(item.link, item)
   return map
-}, {})
+}, new Map<string, NewsItem>())
 
 export const getAllNewsItems = (): NewsItem[] => {
-  return (<NewsItem[]> []).concat(itemsWithIndex)
+  return (<NewsItem[]>[]).concat(itemsWithIndex)
 }
 
-export const getItemByLink = (link: string): NewsItem|undefined => {
-  return indexedByLink.hasOwnProperty(link)
+export const getItemByLink = (link: string): NewsItem | undefined => {
+  return indexedByLink.has(link)
     ? indexedByLink[link]
     : undefined
 }
@@ -35,7 +35,7 @@ export const getLatestNewsItems = (limit: number = 5) => {
   return getAllNewsItems().splice(0, limit)
 }
 
-export const getItemByIndex = (index: number): NewsItem|undefined => {
+export const getItemByIndex = (index: number): NewsItem | undefined => {
   const allItems = getAllNewsItems()
 
   if (index < 0 || index >= allItems.length) {
@@ -45,13 +45,13 @@ export const getItemByIndex = (index: number): NewsItem|undefined => {
   return allItems[index]
 }
 
-export const getNextNewsItem = (item: NewsItem): NewsItem|undefined => {
+export const getNextNewsItem = (item: NewsItem): NewsItem | undefined => {
   return (item.originalIndex !== undefined)
     ? getItemByIndex(item.originalIndex - 1)
     : undefined
 }
 
-export const getPreviousNewsItem = (item: NewsItem): NewsItem|undefined => {
+export const getPreviousNewsItem = (item: NewsItem): NewsItem | undefined => {
   return (item.originalIndex !== undefined)
     ? getItemByIndex(item.originalIndex + 1)
     : undefined
